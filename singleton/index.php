90e45ef01c960118c991dafea4307c0128a08d82
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2019-02-22 01:09:16 +0330

    singleton

--- singleton/index.php
+++ singleton/index.php
@@ -41,3 +41,41 @@ var_dump($instance4->getConnection());
 
 
 
+class Singleton{
+    private static $instance = null;
+
+    private $conn;
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db_name = "behrooz";
+
+    private function __construct(){
+        $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}",$this->user,$this->password);
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
+
+    public static function getInstance(){
+        if(self::$instance==null){
+            self::$instance = new Singleton();
+        }
+        return self::$instance;
+    }
+
+}
+
+
+$instance1 = Singleton::getInstance();
+var_dump($instance1->getConnection());
+
+$instance2 = Singleton::getInstance();
+var_dump($instance2->getConnection());
+
+$instance3 = Singleton::getInstance();
+var_dump($instance3->getConnection());
+
+$instance4 = Singleton::getInstance();
+var_dump($instance4->getConnection());
